354bdb2a7b1a41d77dcaffe95d607c7f5f1b902b
droidjahangir <jahangir.droid@gmail.com>
2021-11-17 02:14:27 +0600

    backend server link updated in heroku

--- src/services/base-http.service.js
+++ src/services/base-http.service.js
@@ -4,6 +4,7 @@ export default class BaseHttpService {
   BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3000';
   _accessToken = null;
 
+  
   constructor(routerStore) {
     this.routerStore = routerStore;
   }
